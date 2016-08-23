a866df9a0ad9de42a355a9fe170b5903f69ca168
Vojtěch Vít <vojtech.vit@frontiersin.org>
2016-08-23 14:12:19 +0200

    fixes in powershell

--- src/RunAll.ps1
+++ src/RunAll.ps1
@@ -8,27 +8,32 @@ if (Get-Job -Name $aspNetCoreUiJobName -ErrorAction SilentlyContinue) {
     Stop-job -Name $aspNetCoreUiJobName
 }
 
+Push-Location $PSScriptRoot
+
+dotnet restore
+
 $aspNetCoreApiJob = Start-Job -Name $aspNetCoreApiJobName -ArgumentList $PSScriptRoot -ScriptBlock { Param($scriptRoot) Set-Location "$scriptRoot\WebServices.AspNetCore"; dotnet run }
 $aspNetCoreUiJob = Start-Job -Name $aspNetCoreUiJobName -ArgumentList $PSScriptRoot -ScriptBlock { Param($scriptRoot) Set-Location "$scriptRoot\Ui"; dotnet run }
 
 do 
 {
-    [string]$jobOutput = $aspNetCoreApiJob | Receive-Job
+    $aspNetCoreApiJob | Receive-Job | Tee-Object -Variable "jobOutput" | Out-Default
     Write-Output $jobOutput
     Start-Sleep -Milliseconds 500
-} while ($jobOutput -notmatch 'Now listening on: ([^ ]+)')
+} while ([string]$jobOutput -notmatch 'Now listening on: ([^ ]+)')
 
 do 
 {
-    [string]$jobOutput = $aspNetCoreUiJob | Receive-Job
+    $aspNetCoreUiJob | Receive-Job | Tee-Object -Variable "jobOutput" | Out-Default
     Write-Output $jobOutput
     Start-Sleep -Milliseconds 500
-} while ($jobOutput -notmatch 'Now listening on: ([^ ]+)')
+} while ([string]$jobOutput -notmatch 'Now listening on: ([^ ]+)')
 
-Push-Location "$PSScriptRoot\DataLoader"
+Push-Location DataLoader
 
 dotnet run load-graph test1 -dir ..\..\samples\input-data -url http://localhost:5001/api/v1/
 
+Pop-Location
 Pop-Location
 
 Write-Host "To stop the running websites, press [Enter]." -ForegroundColor Yellow

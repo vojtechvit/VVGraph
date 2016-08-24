8b035dc7ff03770656d15f02f2ed59e58c778bf6
Vojtěch Vít <vojtech.vit@frontiersin.org>
2016-08-24 16:49:04 +0200

    angular JS implementation refactored to be more angular-native and organized. not finished, though.

--- src/Ui/wwwroot/js/site.js
+++ src/Ui/wwwroot/js/site.js
@@ -1,186 +1,247 @@
 ﻿(function () {
-    var app = angular
-        .module('app', [])
+  var app = angular
+    .module('app', ['ngMessages', 'ngVis'])
 
-        .controller('HomeController',
-            ['$scope', '$http', 'nodesToDataSet', 'edgesToDataSet', 'createGraph', 'refreshGraph',
-            function ($scope, $http, nodesToDataSet, edgesToDataSet, createGraph, refreshGraph) {
-                // Errors
-                $scope.errorMessage = null;
+    .constant('config', {
+      defaultGraphUrl: 'http://localhost:5001/api/v1/graphs/test1',
+      shortestPathEndpoint: '/shortest-path'
+    })
 
-                // Graph
-                $scope.graphUrl = 'http://localhost:5001/api/v1/graphs/test1';
-                $scope.graphName = null;
-                $scope.nodes = null;
-                $scope.edges = null;
-                $scope.network = null;
+    .controller('HomeController',
+      ['$scope',
+      function ($scope) {
+        var self = this;
 
-                $scope.changeGraph = function () {
-                    $http.get($scope.graphUrl)
-                        .success(function (data) {
-                            $scope.graphName = data.name
-                            $scope.nodes = nodesToDataSet(data.nodes);
-                            $scope.edges = edgesToDataSet(data.edges);
-                            $scope.network = createGraph($scope.nodes, $scope.edges);
-                            attachEvents($scope.network);
-                            $scope.errorMessage = null;
-                        })
-                        .error(function (data) {
-                            $scope.errorMessage = 'Could not load the graph information.';
-                        });
+        self.graph = null;
 
-                    $scope.startNodeId = null;
-                    $scope.endNodeId = null;
-                };
+        $scope.$on('graphSelected', function () {
+          self.graph = null;
+        });
 
-                // Load graph
-                $scope.changeGraph();
+        $scope.$on('graphLoaded', function (event, args) {
+          self.graph = args.graph;
+        });
+      }])
 
-                // Shortest path
-                $scope.startNodeId = null;
-                $scope.endNodeId = null;
+    .controller('GraphSelectorController',
+      ['$rootScope', 'config',
+      function ($rootScope, config) {
+        var self = this;
 
-                $scope.findShortestPath = function () {
-                    $http({
-                        url: $scope.graphUrl.trim('/') + '/shortest-path',
-                        method: 'GET',
-                        params: { startNodeId: $scope.startNodeId, endNodeId: $scope.endNodeId }
-                    }).then(function (response) {
-                        refreshGraph($scope.network, $scope.nodes, $scope.edges, response.data);
-                        $scope.network.unselectAll();
-                        $scope.startNodeId = null;
-                        $scope.endNodeId = null;
-                        $scope.errorMessage = null;
-                    }, function (response) {
-                        $scope.errorMessage = 'Could not load the load shortest path.';
-                    });
-                };
+        self.graphUrl = config.defaultGraphUrl;
 
-                function attachEvents(network) {
-                    network.on("selectNode", function (event) {
-                        if (event.nodes.length == 2) {
-                            $scope.startNodeId = event.nodes[0];
-                            $scope.endNodeId = event.nodes[1];
-                            $scope.$apply();
-                        }
-                        else if (event.nodes.length > 2) {
-                            network.unselectAll();
-                            network.selectNodes([$scope.startNodeId, $scope.endNodeId]);
-                        }
-                    });
+        self.selectGraph = function () {
+          $rootScope.$broadcast('graphSelected', { graphUrl: self.graphUrl });
+        }
+      }])
 
-                    network.on("deselectNode", function (event) {
-                        if (event.nodes.length < 2) {
-                            $scope.startNodeId = null;
-                            $scope.endNodeId = null;
-                            $scope.$apply();
-                        }
-                    });
+    .controller('ErrorController',
+      ['$scope',
+      function ($scope) {
+        var self = this;
+
+        self.error = null;
+
+        $scope.$on('error', function (event, args) {
+          self.error = args;
+        });
+
+        $scope.$on('graphSelected', function () {
+          self.error = null;
+        });
+      }])
+
+    .controller('GraphController',
+      ['$rootScope', '$scope', 'graphApi', 'visDataSetHelper',
+      function ($rootScope, $scope, graphApi, visDataSetHelper) {
+        var self = this;
+
+        self.graphData = {};
+
+        self.graphOptions = {
+          interaction: {
+            hover: true,
+            multiselect: true,
+            selectConnectedEdges: false,
+            navigationButtons: true
+          }
+        };
+
+        self.graphEvents = {
+          selectNode: function (event) {
+            $rootScope.$broadcast('nodeSelected', { selectedNodeIds: event.nodes });
+          },
+          deselectNode: function (event) {
+            $rootScope.$broadcast('nodeUnselected', { selectedNodeIds: event.nodes });
+          }};
+
+        $scope.$on('graphSelected', function (event, args) {
+          graphApi.getGraph(args.graphUrl)
+            .then(self.loadGraph, self.graphError);
+        });
+
+        self.loadGraph = function (response) {
+          var graph = response.data;
+          var nodes = visDataSetHelper.mapNodes(graph.nodes);
+          var edges = visDataSetHelper.mapEdges(graph.edges);
+
+          self.graphData = {
+            nodes: nodes,
+            edges: edges
+          };
+
+          $rootScope.$broadcast('graphLoaded', { graph: graph });
+        };
+
+        self.graphError = function () {
+          $rootScope.$broadcast('error', { getGraphApiError: true });
+        };
+
+        $scope.$on('pathFound', function (event, args) {
+          visDataSetHelper.highlightPath(self.graphData, args.pathNodeIds);
+          self.graphData = { nodes: self.graphData.nodes, edges: self.graphData.edges };
+        });
+      }])
+
+    .controller('GraphActionsController',
+      ['$rootScope', '$scope', 'graphApi',
+      function ($rootScope, $scope, graphApi) {
+        var self = this;
+
+        self.graphUrl = null;
+        self.selectedNodeIds = [];
+
+        self.findShortestPath = function () {
+          graphApi.findShortestPath(self.graphUrl, self.selectedNodeIds[0], self.selectedNodeIds[1])
+            .then(self.shortestPathFound, self.shortestPathError);
+        }
+
+        self.shortestPathFound = function (response) {
+          self.selectedNodeIds = [];
+          $rootScope.$broadcast('pathFound', { pathNodeIds: response.data });
+        }
+
+        self.shortestPathError = function () {
+          $rootScope.$broadcast('error', { shortestPathApiError: true });
+        }
+
+        $scope.$on('graphSelected', function (event, args) {
+          self.graphUrl = args.graphUrl;
+          self.selectedNodeIds = [];
+        });
+
+        $scope.$on('nodeSelected', function (event, args) {
+          /*if (args.selectedNodeIds.length > 2) {
+            //$rootScope.$broadcast('setGraphNodes', { nodeIds: self.selectedNodeIds });
+            self.selectedNodeIds = args.selectedNodeIds.slice();
+          }
+          else {
+            self.selectedNodeIds = args.selectedNodeIds.slice();
+          }*/
+          $scope.$apply(function () { self.selectedNodeIds = args.selectedNodeIds.slice(); });
+        });
+
+        $scope.$on('nodeUnselected', function (event, args) {
+          $scope.$apply(function () { self.selectedNodeIds = args.selectedNodeIds.slice(); });
+        });
+      }])
+
+    .factory('graphApi',
+      ['$http', 'config',
+      function ($http, config) {
+        return {
+          getGraph: function (graphUrl) {
+            return $http({
+              url: graphUrl,
+              method: 'GET'
+            })
+          },
+          findShortestPath: function (graphUrl, startNodeId, endNodeId) {
+            return $http({
+              url: graphUrl.trim('/') + config.shortestPathEndpoint,
+              method: 'GET',
+              params: { startNodeId: startNodeId, endNodeId: endNodeId }
+            })
+          }
+        }}])
+
+    .factory('visDataSetHelper',
+    [
+    function () {
+      return {
+        mapNodes: function (nodes) {
+          var dsNodes = new vis.DataSet();
+
+          nodes.forEach(function (n) {
+            dsNodes.add([{
+              id: n.id,
+              label: n.label,
+              shape: 'circle',
+              color: {
+                border: '#000000',
+                background: '#ffffff',
+                hover: {
+                  border: '#000000',
+                  background: '#dddddd'
                 }
-            }])
+              }
+            }]);
+          });
 
-        .factory("nodesToDataSet", [function () {
-            return function (nodes) {
-                var dsNodes = new vis.DataSet();
-                nodes.forEach(function (n) {
-                    dsNodes.add([{
-                        id: n.id,
-                        label: n.label,
-                        shape: 'circle',
-                        color: {
-                            border: '#000000',
-                            background: '#ffffff',
-                            hover: {
-                                border: '#000000',
-                                background: '#dddddd'
-                            }
-                        }
-                    }]);
-                });
+          return dsNodes;
+        },
+        mapEdges: function (edges) {
+          var dsEdges = new vis.DataSet();
 
-                return dsNodes;
+          edges.forEach(function (e) {
+            dsEdges.add([{
+              from: e.startNodeId,
+              to: e.endNodeId,
+              color: {
+                color: '#000000',
+                inherit: false
+              },
+              width: 1
+            }]);
+          });
+
+          return dsEdges;
+        },
+        highlightPath: function(data, pathNodeIds) {
+          data.nodes.forEach(function (n) {
+            if (pathNodeIds.indexOf(n.id) != -1) {
+              var border = '#32a800';
+              var background = '#e6ffdb';
             }
-        }])
-
-        .factory("edgesToDataSet", [function () {
-            return function (edges) {
-                var dsEdges = new vis.DataSet();
-
-                edges.forEach(function (e) {
-                    dsEdges.add([{
-                        from: e.startNodeId,
-                        to: e.endNodeId,
-                        color: {
-                            color: '#000000',
-                            inherit: false
-                        },
-                        width: 1
-                    }]);
-                });
-
-                return dsEdges;
+            else {
+              var border = '#000000';
+              var background = '#ffffff';
             }
-        }])
 
-        .factory("createGraph", [function () {
-            return function (nodes, edges) {
-                container = document.getElementById('graph-container');
+            n.color.border = border;
+            n.color.background = background;
+          });
 
-                var data = {
-                    nodes: nodes,
-                    edges: edges
-                };
+          data.edges.forEach(function (e) {
+            var indexOfFrom = pathNodeIds.indexOf(e.from);
+            var indexOfTo = pathNodeIds.indexOf(e.to);
 
-                var options = {
-                    interaction: {
-                        hover: true,
-                        multiselect: true,
-                        selectConnectedEdges: false,
-                        navigationButtons: true
-                    }
-                };
-
-                return new vis.Network(container, data, options);
+            if ((indexOfFrom != -1 && indexOfTo != -1)
+                    && (indexOfTo == indexOfFrom + 1
+                        || indexOfTo == indexOfFrom - 1)) {
+              var color = '#32a800';
+              var width = 2;
             }
-        }])
-
-        .factory("refreshGraph", [function () {
-            return function (network, nodes, edges, shortestPath) {
-                nodes.forEach(function (n) {
-                    if (shortestPath.indexOf(n.id) != -1) {
-                        var border = '#32a800';
-                        var background = '#e6ffdb';
-                    }
-                    else {
-                        var border = '#000000';
-                        var background = '#ffffff';
-                    }
-
-                    n.color.border = border;
-                    n.color.background = background;
-                });
-
-                edges.forEach(function (e) {
-                    var indexOfFrom = shortestPath.indexOf(e.from);
-                    var indexOfTo = shortestPath.indexOf(e.to);
-
-                    if ((indexOfFrom != -1 && indexOfTo != -1)
-                            && (indexOfTo == indexOfFrom + 1
-                                || indexOfTo == indexOfFrom - 1)) {
-                        var color = '#32a800';
-                        var width = 2;
-                    }
-                    else {
-                        var color = '#000000';
-                        var width = 1;
-                    }
-
-                    e.color.color = color;
-                    e.width = width;
-                });
-
-                network.setData({ nodes: nodes, edges: edges });
-                network.redraw();
+            else {
+              var color = '#000000';
+              var width = 1;
             }
-        }]);
+
+            e.color.color = color;
+            e.width = width;
+          });
+
+          return data;
+        }
+      }}])
 })();
